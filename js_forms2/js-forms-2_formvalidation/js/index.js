e81c3c4bbfe592c04e63973c91766e8b1f7257d0
meShailu <123243572+meShailu@users.noreply.github.com>
2023-07-13 18:29:20 +0200

    form validation completed

--- js_forms2/js-forms-2_formvalidation/js/index.js
+++ js_forms2/js-forms-2_formvalidation/js/index.js
@@ -1,12 +1,33 @@
+// console.clear();
+
+// const form = document.querySelector('[data-js="form"]');
+// const checkbox = document.getElementById("terms-of-service");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.target);
+//   const data = Object.fromEntries(formData);
+
+//   console.log(data);
+//   if (!checkbox.checked) {
+//     alert("Please agree to the terms and conditions.");
+//   }
+// });
+
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const checkbox = document.getElementById("terms-of-service");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   console.log(data);
+
+  if (!checkbox.checked) {
+    alert("Please agree to the terms and conditions.");
+  }
 });
