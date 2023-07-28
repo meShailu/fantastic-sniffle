c969efc1479e24e0d6c0cf8bdd7cff894ddfcf28
meShailu <123243572+meShailu@users.noreply.github.com>
2023-07-28 13:03:50 +0200

    Third challenge

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Why do you think Koalas suck!</h2>
+      <label htmlFor="answer">Answer:</label>
+      <input id="answer"></input>
+      <a className="article__link" href="https://en.wikipedia.org/wiki/Koala">
+        learn about Koalas here!
+      </a>
+    </article>
+  );
 }
