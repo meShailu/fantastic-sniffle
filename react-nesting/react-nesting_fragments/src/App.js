ac59ef0bca48036fa920b1d9b0fc863656eb653c
meShailu <123243572+meShailu@users.noreply.github.com>
2023-07-31 11:04:05 +0200

    second challenge for boxes

--- react-nesting/react-nesting_fragments/src/App.js
+++ react-nesting/react-nesting_fragments/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./styles.css";
-import Boxes from "./components/Boxes";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <main className="flex-container">
-      <Boxes />
+      <div>
+        <h1>App Component</h1>
+        <Boxes />
+      </div>
     </main>
+  );
+}
+
+function Boxes() {
+  return (
+    <div>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </div>
   );
 }
