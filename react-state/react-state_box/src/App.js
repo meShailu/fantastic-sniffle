ef1de39fe5ef789b8d48b18cb6b25766e504d9ba
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-01 10:23:10 +0200

    fisrt challenge

--- react-state/react-state_box/src/App.js
+++ react-state/react-state_box/src/App.js
@@ -1,18 +1,24 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  // let isActive = false;
+  const [active, setActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    // isActive = !isActive;
+    setActive(!active);
+
     // Check that the value changes correctly.
-    console.log(isActive);
+    console.log(active);
   }
 
   return (
     <main>
-      <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <div className={`box ${active ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        {active ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
