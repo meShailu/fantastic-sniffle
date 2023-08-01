6986fd2d5fa8959e7644bbe5972d7aa8dde3bca8
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-01 12:29:39 +0200

    challenges

--- react-state/react-state_emoji-checker/src/App.js
+++ react-state/react-state_emoji-checker/src/App.js
@@ -1,67 +1,119 @@
+// import React from "react";
+// import "./styles.css";
+// import { useState } from "react";
+
+// export default function App() {
+//   //let code = "?";
+//   const [code, setCode] = useState("");
+
+const validCode = "🐡🐠🐋";
+
+//   const handleClick = (emoji) => {
+//     setCode((prevCode) => [prevCode + emoji]);
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="button-container">
+//         <button
+//           type="button"
+//           onClick={() => {
+//             handleClick("🐡");
+//             console.log("Update Code!");
+//           }}
+//         >
+//           <span role="img" aria-label="Pufferfish">
+//             🐡
+//           </span>
+//         </button>
+//         <button
+//           type="button"
+//           onClick={() => {
+//             handleClick("🐋");
+//             console.log("Update Code!");
+//           }}
+//         >
+//           <span role="img" aria-label="Whale">
+//             🐋
+//           </span>
+//         </button>
+//         <button
+//           type="button"
+//           onClick={() => {
+//             handleClick("🐠");
+
+//             console.log("Update Code!");
+//           }}
+//         >
+//           <span role="img" aria-label="Clownfish">
+//             🐠
+//           </span>
+//         </button>{" "}
+//       </div>
+
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setCode("");
+//           console.log("Reset Code!");
+//         }}
+//       >
+//         Reset
+//       </button>
+//       <h2>{code}</h2>
+
+//       {code === validCode && <p>Valid code!</p>}
+//     </div>
+//   );
+// }
 import React from "react";
 import "./styles.css";
-import { useState } from "react";
-
 export default function App() {
-  //let code = "?";
-  const [code, setCode] = useState("");
-
-  const validCode = "🐡🐠🐋";
-
-  const handleClick = (emoji) => {
-    setCode((prevCode) => [prevCode + emoji]);
-  };
-
+  let code = "?";
+  // const validCode = ":blowfish::tropical_fish::whale2:";
   return (
     <div className="container">
       <div className="button-container">
         <button
           type="button"
           onClick={() => {
-            handleClick("🐡");
             console.log("Update Code!");
           }}
         >
           <span role="img" aria-label="Pufferfish">
-            🐡
+            :blowfish:
           </span>
         </button>
         <button
           type="button"
           onClick={() => {
-            handleClick("🐋");
             console.log("Update Code!");
           }}
         >
           <span role="img" aria-label="Whale">
-            🐋
+            :whale2:
           </span>
         </button>
         <button
           type="button"
           onClick={() => {
-            handleClick("🐠");
-
             console.log("Update Code!");
           }}
         >
           <span role="img" aria-label="Clownfish">
-            🐠
+            :tropical_fish:
           </span>
         </button>{" "}
       </div>
-
       <button
         type="button"
         onClick={() => {
-          setCode("");
           console.log("Reset Code!");
         }}
       >
         Reset
       </button>
       <h2>{code}</h2>
-
       {code === validCode && <p>Valid code!</p>}
     </div>
   );
