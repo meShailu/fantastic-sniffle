49443c81690d512eb6d34407aea3d8e9c3deaf74
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-12 21:45:06 +0200

    star-wars

---
+++ react-data-fetching/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,36 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data</p>;
+  }
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
