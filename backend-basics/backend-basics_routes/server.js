bcc47e9edbd96027b83d6136489cfab70bf9225e
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-21 10:52:07 +0200

    second cahllenge

--- backend-basics/backend-basics_routes/server.js
+++ backend-basics/backend-basics_routes/server.js
@@ -1,3 +1,16 @@
 import { createServer } from "node:http";
 
+export const server = createServer((req, res) => {
+  if (req.url === "/api/fish/1") {
+    res.statusCode = 200;
+    res.end("Shrimp");
+  } else if (req.url === "/api/fish/2") {
+    res.statusCode = 200;
+    res.end("Anemonefish");
+  } else {
+    res.statusCode = 404;
+    res.end("Not found");
+  }
+});
+
 // export const server = …
