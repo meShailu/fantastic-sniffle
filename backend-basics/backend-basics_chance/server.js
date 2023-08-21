b0e785687e48f460559bf527c12c215906345f61
meShailu <123243572+meShailu@users.noreply.github.com>
2023-08-21 11:08:46 +0200

    third challenge

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,14 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+export const server = createServer((req, res) => {
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+
+  const response = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+  res.statusCode = 200;
+  res.end(response);
+});
+const chance = new Chance();
